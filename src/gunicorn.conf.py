6da891ea8c816725271fdf18f56fe53f26ff0d99
Pamela Fox <pamela.fox@gmail.com>
2024-07-24 20:46:12 +0000

    Port

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -3,7 +3,7 @@ import multiprocessing
 max_requests = 1000
 max_requests_jitter = 50
 log_file = "-"
-bind = "0.0.0.0:8888"
+bind = "0.0.0.0:8000"
 workers = (multiprocessing.cpu_count() * 2) + 1
 
 worker_class = "my_uvicorn_worker.MyUvicornWorker"
